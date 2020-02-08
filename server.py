f8087c6d824427bdc9cf65faa9dbc79a6f3b884d
Jessica <jessica.ho.jh@gmail.com>
2020-02-07 16:51:09 -0800

    added login, register, and logout route

--- server.py
+++ server.py
@@ -21,13 +21,70 @@ app.secret_key = "ABC"
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show form for user signup."""
+
+    return render_template("register_form.html")
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process Registration."""
+
+    email = request.form["email"]
+    password = request.form["password"]
+
+    new_user = User(email=email, password=password, status=status)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Login form."""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process login."""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("No such user")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+    return redirect(f"/")
+
+
+@app.route('/logout')
+def logout():
+    """Log out."""
+
+    del session["user_id"]
+    flash("Logged Out.")
+    return redirect("/")
 
 @app.route('/')
 def homepage():
     """Homepage."""
 
     return render_template("homepage.html")
-
 
 @app.route("/items")
 def items_list():
